668fc706054b0743e22e31f687eebc70698f70b9
berart <berart@protonmail.com>
2023-01-04 18:42:02 +0200

    update

--- tools.sh
+++ tools.sh
@@ -11,6 +11,7 @@ sudo apt-get install -y python2 ;
 sudo apt-get install -y python3 ;
 sudo apt-get install -y python-pip;
 sudo apt-get install -y python3-pip;
+reset;
 
 # sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 # sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
@@ -22,10 +23,14 @@ bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEA
 echo '# Set PATH, MANPATH, etc., for Homebrew.' >> $HOME/.zprofile;
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.zprofile;
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)";
+reset;
+
 sudo brew install -s -f gcc ;
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash;
 echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.bashrc;
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.bashrc;
+reset;
+
 sudo apt-get install -y caca-utils;
 sudo brew install -s -f ghostscript ;
 sudo brew install -s -f imagemagick ;
@@ -42,8 +47,8 @@ sudo apt-get install -y calibre ;
 sudo apt-get install -y mediainfo ;
 sudo apt-get install -y odt2txt ;
 sudo apt-get install -y jq ;
-sudo pip install jupyterlab --quiet;
-sudo pip install nbconvert --quiet;
+pip install jupyterlab --quiet;
+pip install nbconvert --quiet;
 sudo apt-get install -y fontforge ;
 sudo apt-get install -y openscad;
 sudo apt-get install -y python3-neovim;
@@ -53,17 +58,26 @@ sudo brew install -s -f lazydocker;
 sudo brew install -s -f bpytop;
 sudo brew install -s -f ncdu;
 sudo brew install -s -f hashicorp/tap/terraform-ls;
-sudo pip3 install jedi --quiet;
+pip3 install jedi --quiet;
 sudo apt-get install -y exuberant-ctags;
 
 #check is npm aveiloble
 sudo nvm install --lts;
 sudo npm i --silent --global pyright;
 sudo npm install --silent --global yarn;
+reset;
 
-#Install Dotnet
+#Install Dotnet 
 wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb;
 sudo dpkg -i packages-microsoft-prod.deb;
 rm packages-microsoft-prod.deb;
+reset;
+
 sudo apt-get install -y dotnet-sdk-7.0;
+reset;
+
 sudo dotnet tool install --global csharp-ls;
+
+#brew not found
+#dotnet-sdk-7.0 not found
+#dotnet not found
--- zsh.sh
+++ zsh.sh
@@ -19,9 +19,16 @@ echo 'export PATH="/usr/local/bin/w3mimgdisplayr:$PATH"' >> $HOME/.zshrc;
 sudo apt-get update && sudo apt-get upgrade && sudo apt-get install zsh;
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
+
+echo 'zinit load zdharma-continuum/history-search-multi-word' >> $HOME/.zshrc;
+echo 'zinit light zsh-users/zsh-autosuggestions' >> $HOME/.zshrc;
+echo 'zinit light zsh-users/zsh-completions' >> $HOME/.zshrc;
+echo 'zinit light zdharma-continuum/fast-syntax-highlighting' >> $HOME/.zshrc;
+
 #install theme
 sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt";
 sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme";
+#Install zinit
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)";
 
 echo 'source $ZSH/oh-my-zsh.sh' >> $HOME/.zshrc;
@@ -29,10 +36,6 @@ echo 'source $ZSH/oh-my-zsh.sh' >> $HOME/.zshrc;
 echo 'export VISUAL=nvim;' >> $HOME/.zshrc;
 echo 'export EDITOR=nvim;' >> $HOME/.zshrc;
 
-echo 'zinit load zdharma-continuum/history-search-multi-word' >> $HOME/.zshrc;
-echo 'zinit light zsh-users/zsh-autosuggestions' >> $HOME/.zshrc;
-echo 'zinit light zsh-users/zsh-completions' >> $HOME/.zshrc;
-echo 'zinit light zdharma-continuum/fast-syntax-highlighting' >> $HOME/.zshrc;
 
 echo 'export NVM_DIR="$HOME/.nvm"' >> $HOME/.zshrc;
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $HOME/.zshrc;
