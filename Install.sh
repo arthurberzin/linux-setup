7489a17220ecdb150adf673ca3f912f13bea26d8
berart <berart@protonmail.com>
2023-01-04 10:52:55 +0200

    update

--- Install.sh
+++ Install.sh
@@ -1,1 +1,5 @@
 bash <(curl -s https://raw.githubusercontent.com/arthurberzin/linux-setup/main/tools.sh)
+bash <(curl -s https://raw.githubusercontent.com/arthurberzin/linux-setup/main/zsh.sh)
+bash <(curl -s https://raw.githubusercontent.com/arthurberzin/linux-setup/main/ranger.sh)
+bash <(curl -s https://raw.githubusercontent.com/arthurberzin/linux-setup/main/nvim.sh)
+
