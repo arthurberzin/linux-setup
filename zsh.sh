b240aeb1935f9ba72268f8d546517f37e647bcc8
berart <berart@protonmail.com>
2023-01-05 12:09:16 +0200

    update

--- zsh.sh
+++ zsh.sh
@@ -15,6 +15,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 #https://github.com/jonmosco/spaceship-zsh-theme
 curl -o - https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh | zsh
+
 #https://github.com/spaceship-prompt/spaceship-prompt
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
