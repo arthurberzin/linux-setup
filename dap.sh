e8da19b9c82bfc74460d33aa793ff8af5ab9ccf5
berart <berart@protonmail.com>
2023-01-07 17:45:36 +0200

    update

--- dap.sh
+++ dap.sh
@@ -3,7 +3,7 @@ echo "==============================================       DAP     =============
 echo "================================================================================================================"
 #https://microsoft.github.io/debug-adapter-protocol/overview
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-source $HOME/.profile 
+source $HOME/.profile
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -14,10 +14,8 @@ echo " "
 echo "------------------------ @vscode/debugadapter  ------------------------"
 npm install --silent -g @vscode/debugadapter
 
-
-
 echo "------------------------ node-debug2  ------------------------"
 npm install --silent -g node-debug2
 
 echo "------------------------ node-debug2  ------------------------"
-npm  --silent -g vscode-chrome-debug-core
+npm i -g vscode-chrome-debug-core
--- lsp.sh
+++ lsp.sh
@@ -3,7 +3,7 @@ echo "==============================================       LSP     =============
 echo "================================================================================================================"
 #https://microsoft.github.io/language-server-protocol/overviews/lsp/overview/
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-source $HOME/.profile 
+source $HOME/.profile
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -12,26 +12,22 @@ export NVM_DIR="$HOME/.nvm"
 echo " "
 
 echo "------------------------ Install python ls ------------------------"
-pip install python-language-server  --quiet 
-
+pip install python-language-server --quiet
 
 echo "------------------------ Install csharp-ls  ------------------------"
-sudo dotnet tool install --global csharp-ls 
+sudo dotnet tool install --global csharp-ls
 sudo dotnet tool install --global csharp-language-server-protocol
 
 #git clone git://github.com/OmniSharp/omnisharp-vim ~/.vim/pack/plugins/start/omnisharp-vim
 
 echo "=========================terraform-ls==================================="
-brew install -f hashicorp/tap/terraform-ls 
-
+brew install -f hashicorp/tap/terraform-ls
 
 echo "========================= lua-language-server  ==================================="
 brew install -f lua-language-server
 
-
 echo "========================= typescript typescript-language-server  ==================================="
 npm install --silent -g typescript typescript-language-server
-
 
 echo "========================= HTML/CSS/JSON/ESLint  ==================================="
 npm i --silent -g vscode-languageserver
@@ -47,30 +43,23 @@ npm install --silent --global vscode-css-languageserver-bin
 echo "========================= bash-language-server ==================================="
 npm i --silent -g bash-language-server
 
-
 echo "========================= yaml-language-server ==================================="
 brew install -f yaml-language-server
-
 
 echo "========================= sql-language-server ==================================="
 brew install -f sql-language-server
 
-
 echo "========================= azure-pipelines-language-server ==================================="
 npm i --silent -g azure-pipelines-language-server
-
 
 echo "========================= vim language-server ==================================="
 yarn global add vim-language-server
 
-
 echo "========================= dockerfile-language-server ==================================="
 npm install --silent -g dockerfile-language-server-nodejs
 
-
-
-echo "========================= xml language-server ==================================="
-git clone https://github.com/microclimate-dev2ops/xml-language-server.git
-cd xml-language-server/server/xml-server
-./mvnw clean package
-cd ~
+echo "========================= xml language-server need auth ?!  ==================================="
+# https://github.com/microclimate-dev2ops/xml-language-server.git
+#cd xml-language-server/server/xml-server
+#./mvnw clean package
+#cd ~
