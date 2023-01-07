bfc4c40401dad3e65205dd2ed7b9490b27ec8fd3
berart <berart@protonmail.com>
2023-01-07 16:41:43 +0200

    update

--- nvim.sh
+++ nvim.sh
@@ -4,8 +4,8 @@ echo "==========================================================================
 
 source "$HOME/.cargo/env"
 
-#brew install -f --HEAD neovim
-brew install -f neovim
+brew install -f --HEAD neovim
+#brew install -f neovim
 
 #curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage &&
 #chmod u+x nvim.appimage &&
@@ -26,37 +26,45 @@ git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
 
 
-
 echo "============================ Setting Up =======================================#"
 #https://astronvim.github.io/#-setup
 mkdir ~/.config/nvim/lua/user/
 curl -o ~/.config/nvim/lua/user/init.lua https://raw.githubusercontent.com/arthurberzin/linux-setup/main/init.lua
 
 
-echo "========================= LspInstall  ==================================="
+#echo "========================= LspInstall  ==================================="
 #https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
 # nvim +':LspInstall pyright angularls arduino_language_server asm_lsp astro clangd csharp_ls omnisharp cssls dockerls eslint graphql html jsonls tsserver sumneko_lua marksman sqls tailwindcss terraformls lemminx yamlls'
 
 
 
 
-echo "========================= TsInstall  ==================================="
+#echo "========================= TsInstall  ==================================="
 #nvim-treesitter
 #https://astronvim.github.io/#install-language-parser
 #https://github.com/nvim-treesitter/nvim-treesitter
 #https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
 # nvim +':TSInstall bash c c_sharp cpp css dockerfile git_rebase gitattributes gitcommit gitignore javascript html json lua markdown meson python sql terraform typescript yaml yang vim tsx'
 
-echo "========================= DapInstall  ==================================="
+#echo "========================= DapInstall  ==================================="
 #https://github.com/ravenxrz/DAPInstall.nvim#list-of-debuggers
 # nvim +':DapInstall ccppr_vsc lua dnetcs chrome jsnode markdown' +qa
 
 
-echo "========================= PackerSync  ==================================="
-nvim +':PackerClean' +qa
-nvim +':PackerSync' +qa
+#echo "========================= PackerSync  ==================================="
+
+#nvim +':PackerSync'
 
 
-echo "========================= AstroUpdate  ==================================="
-nvim +':AstroUpdate' +qa
+#echo "========================= AstroUpdate  ==================================="
+#nvim +':AstroUpdate'
 
+
+
+
+echo "-----------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+                 Please run < nvim +':PackerSync' > to start install all plugins.
+           After it please run < nvim +':Mason' > and install all needed 'Dap' servers.
+-----------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------"
